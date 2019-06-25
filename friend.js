97da505e4961187cb859722816d9ef1ba4f456d6
Lili Boxer <ninaliboxer@gmail.com>
2019-06-25 16:52:47 -0700

    random num function

--- friend.js
+++ friend.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const { join } = require('path');
 
 
-const txtContent = ['dinosaur', 'dragon', 'drake'];
+const txtContent = ['dinosaur', 'dragon', 'drake', 'element', 'goblin', 'planeswalker', 'shade', 'weird', 'wizard'];
+
+const randomNum = Math.floor(Math.random() * 10);
+console.log(randomNum);
+
+
 
 for(let i = 0; i < 15; i++) {
-  fs.writeFile(join(__dirname, `./${i}.txt`,));
-  
+  fs.writeFile(join(__dirname, `./files/${i}.txt`, `${}`));
 }
