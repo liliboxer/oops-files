549209078fa9e7101b5621aa87af5f0b75d23654
Lili Boxer <ninaliboxer@gmail.com>
2019-06-25 16:53:46 -0700

    random txt content working

--- friend.js
+++ friend.js
@@ -5,10 +5,10 @@ const { join } = require('path');
 const txtContent = ['dinosaur', 'dragon', 'drake', 'element', 'goblin', 'planeswalker', 'shade', 'weird', 'wizard'];
 
 const randomNum = Math.floor(Math.random() * 10);
-console.log(randomNum);
 
+const randomTxtContent = txtContent[randomNum];
+console.log(randomTxtContent);
 
-
-for(let i = 0; i < 15; i++) {
-  fs.writeFile(join(__dirname, `./files/${i}.txt`, `${}`));
-}
+// for(let i = 0; i < 15; i++) {
+//   fs.writeFile(join(__dirname, `./files/${i}.txt`, `${}`));
+// }
