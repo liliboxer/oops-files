5844d4c83c12409e6733bac981a36167a7b54734
Lili Boxer <ninaliboxer@gmail.com>
2019-06-25 16:55:27 -0700

    refactor for cleaner randomFileContent

--- friend.js
+++ friend.js
@@ -2,13 +2,10 @@ const fs = require('fs');
 const { join } = require('path');
 
 
-const txtContent = ['dinosaur', 'dragon', 'drake', 'element', 'goblin', 'planeswalker', 'shade', 'weird', 'wizard'];
-
-const randomNum = Math.floor(Math.random() * 10);
-
-const randomTxtContent = txtContent[randomNum];
-console.log(randomTxtContent);
+const fileContent = ['dinosaur', 'dragon', 'drake', 'element', 'goblin', 'planeswalker', 'shade', 'weird', 'wizard'];
+const randomFileContent = fileContent[Math.floor(Math.random() * 10)];
+console.log(randomFileContent);
 
 // for(let i = 0; i < 15; i++) {
-//   fs.writeFile(join(__dirname, `./files/${i}.txt`, `${}`));
+//   fs.writeFile(join(__dirname, `./files/${i}.txt`, `${randomFileContent}`));
 // }
