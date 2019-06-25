06a3cb07c3cccea61d877aa8f9965d15bcf558f8
Lili Boxer <ninaliboxer@gmail.com>
2019-06-25 16:59:13 -0700

    files randomally made!

--- friend.js
+++ friend.js
@@ -4,8 +4,10 @@ const { join } = require('path');
 
 const fileContent = ['dinosaur', 'dragon', 'drake', 'element', 'goblin', 'planeswalker', 'shade', 'weird', 'wizard'];
 const randomFileContent = fileContent[Math.floor(Math.random() * 10)];
-console.log(randomFileContent);
 
-// for(let i = 0; i < 15; i++) {
-//   fs.writeFile(join(__dirname, `./files/${i}.txt`, `${randomFileContent}`));
-// }
+for(let i = 0; i < 15; i++) {
+  fs.writeFile(join(__dirname, `./files/${i}.txt`), `${randomFileContent}`, 'utf8', (err) => {
+    if(err) console.log(err);
+    console.log('files made');
+  });
+}
